a5eefb373707eb156a85731eb1139c0793c213b2
Pratham-Gauswami <gauswamp@uwindsor.ca>
2024-09-25 18:56:05 -0400

    Adding the extension to chrome

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 let myLeads = []
 const inputEl = document.getElementById("input-el")
 const button = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
 
 //Approach one
 // function save(){
@@ -11,5 +12,22 @@ const button = document.getElementById("input-btn")
 //Approach two
 button.addEventListener("click", function(){
     button.textContent = "Button Clicked!"
-    console.log(button)
+    myLeads.push(inputEl.value)
+    console.log(myLeads)
+    inputEl.value = ""
+    renderLeads()
 })
+
+function renderLeads() {
+    let listItems = "" //variable to hold the list items
+    for (let i = 0; i < myLeads.length; i++) {
+        // listItems += "<li><a target='_blank' href = '" + myLeads[i] + "'>" + myLeads[i] + "</a></li>"
+        //Using Template on the next line
+        listItems += `
+        <li>
+        <a target='_blank' href = '${myLeads[i]}'>${myLeads[i]}</a>
+        </li> `
+        console.log(myLeads[i])
+    }
+    ulEl.innerHTML = listItems
+}
