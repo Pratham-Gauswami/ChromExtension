43ab408b43907d0c4dfff76d8b2762421ec8fe4a
Pratham-Gauswami <gauswamp@uwindsor.ca>
2024-09-26 16:16:01 -0400

    Final version update

--- index.js
+++ index.js
@@ -1,7 +1,41 @@
 let myLeads = []
+let oldLeads = []
 const inputEl = document.getElementById("input-el")
 const button = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
+const deleteBtn = document.getElementById("delete-btn")
+const saveTab = document.getElementById("save-btn")
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+
+if(leadsFromLocalStorage){
+    myLeads = leadsFromLocalStorage
+    renderLeads(myLeads)
+}
+
+// const tabs = [
+//     {url: "https://www.linkedin.com/in/pratham-goswami-331aa7240/"}
+// ]
+
+saveTab.addEventListener("click", function(){
+    chrome.tabs.query({active: true}, function(tabs){
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads))
+        renderLeads(myLeads)
+    })
+})
+
+function renderLeads(leads) {
+    let listItems = "" //variable to hold the list items
+    for (let i = 0; i < leads.length; i++) {
+        // listItems += "<li><a target='_blank' href = '" + myLeads[i] + "'>" + myLeads[i] + "</a></li>"
+        //Using Template on the next line
+        listItems += `
+        <li>
+        <a target='_blank' href = '${leads[i]}'>${leads[i]}</a>
+        </li> `
+    }
+    ulEl.innerHTML = listItems
+}
 
 //Approach one
 // function save(){
@@ -9,25 +43,21 @@ const ulEl = document.getElementById("ul-el")
 //     console.log(button)
 // }
 
-//Approach two
-button.addEventListener("click", function(){
-    button.textContent = "Button Clicked!"
-    myLeads.push(inputEl.value)
-    console.log(myLeads)
-    inputEl.value = ""
-    renderLeads()
+deleteBtn.addEventListener("click", function(){
+    console.log("double clicked!")
+    localStorage.clear()
+    myLeads = []
+    renderLeads(myLeads)
 })
 
-function renderLeads() {
-    let listItems = "" //variable to hold the list items
-    for (let i = 0; i < myLeads.length; i++) {
-        // listItems += "<li><a target='_blank' href = '" + myLeads[i] + "'>" + myLeads[i] + "</a></li>"
-        //Using Template on the next line
-        listItems += `
-        <li>
-        <a target='_blank' href = '${myLeads[i]}'>${myLeads[i]}</a>
-        </li> `
-        console.log(myLeads[i])
-    }
-    ulEl.innerHTML = listItems
-}
+//Approach two
+button.addEventListener("click", function(){
+    // button.textContent = "Button Clicked!"
+    myLeads.push(inputEl.value)
+    inputEl.value = ""
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+    //To check if it is working or not
+    console.log(localStorage.getItem("myLeads"))
+
+    renderLeads(myLeads)
+})
